f07fd3df8204a21e81aa623429157f3135e57a5c
BenatEiguren <benat.eigurenzu@elorrieta-errekamari.com>
2024-02-29 09:03:24 +0100

    modified:   "contrase\303\261as.txt" 	modified:   main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from criptro import *
 import tkinter as tk
 
+cipher = generate_key_from_password("1")
+
+def assignCipher(entry_widget):
+    text = entry_widget.get()
+    # Perform operations with the text (replace this with your actual logic)
+    cipher = generate_key_from_password(text)
+
+def decryptPasswords(cipher):
+    return leer(cipher)
+
 def main():
 
     # cipher = generate_key_from_password(password)  
     root = tk.Tk()
-    root.title("Split Screen GUI")
+    root.title("Encriptador y Decriptador de contraseñas")
 
     # Left side widgets
     left_frame = tk.Frame(root)
@@ -42,13 +52,17 @@ def main():
     password_entry_right = tk.Entry(right_frame, show="*")
     password_entry_right.grid(row=1, column=1)
 
-    accept_button_right = tk.Button(right_frame, text="OK")
+    accept_button_right = tk.Button(right_frame, text="OK",command=assignCipher(password_entry_right))
     accept_button_right.grid(row=2, column=0, columnspan=2, pady=10)
 
     # Text widget on the right side
     result_text = tk.Text(right_frame, height=10, width=30)
     result_text.grid(row=3, column=0, columnspan=2)
     result_text.config(state=tk.DISABLED)
+
+    refresh_text = tk.Button(right_frame,text="Actualizar", command=decryptPasswords(cipher=cipher))
+    refresh_text.grid(row=4, column=0, columnspan=2)
+    
 
     root.mainloop()
 
